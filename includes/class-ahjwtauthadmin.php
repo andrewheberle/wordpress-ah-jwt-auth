3dc67e571f9f6da2dc5f576675f1ebf3b8b041c7
Andrew Heberle <andrewheberle@users.noreply.github.com>
2021-10-10 19:16:53 +0800

    Clean ups based on WPCS linting  (#3)
    Large number of clean-ups and changes based on WordPress Coding Standards linting

---
+++ includes/class-ahjwtauthadmin.php
@@ -0,0 +1,200 @@
+<?php
+/**
+ *
+ * AhJwtAuthAdmin handles the admin side of the plugin.
+ *
+ * @file
+ * @category AhJwtAuthAdmin
+ * @package AhJwtAuth
+ */
+
+namespace AhJwtAuth;
+
+/**
+ *
+ * This class handles the settings/admin sie of the plugin to
+ * allow configuration of the plugin.
+ *
+ * @category Class
+ * @package AhJwtAuth
+ */
+class AhJwtAuthAdmin {
+	/**
+	 * Sets up the class ready for use
+	 *
+	 * @return void
+	 */
+	public function __construct() {
+		add_action( 'admin_init', array( $this, 'register_settings_action' ) );
+		add_action( 'admin_menu', array( $this, 'options_menu_action' ) );
+	}
+
+	/**
+	 * Sets up the plugin options page action
+	 *
+	 * @return void
+	 */
+	public function options_menu_action() {
+		add_options_page(
+			'AH JWT Auth Options',
+			'AH JWT Auth',
+			'manage_options',
+			'ahjwtauth-sign-in-widget',
+			array( $this, 'options_page_action' )
+		);
+	}
+
+	/**
+	 * Includes the option page if the user has permissions
+	 *
+	 * @return void
+	 */
+	public function options_page_action() {
+		if ( current_user_can( 'manage_options' ) ) {
+			include( plugin_dir_path( __FILE__ ) . '../templates/options-form.php' );
+		} else {
+			wp_die( 'You do not have sufficient permissions to access this page.' );
+		}
+	}
+
+	/**
+	 * Displays a text input field on the options page
+	 *
+	 * @param string $option_name the name of the plugin option.
+	 * @param string $type the type of input field.
+	 * @param string $placeholder the placeholder text to include in the input field.
+	 * @param string $description description to display under option.
+	 * @return void
+	 */
+	public function options_page_text_input_action( $option_name, $type, $placeholder = false, $description = false ) {
+		$option_value = get_option( $option_name, '' );
+		printf(
+			'<input type="%s" id="%s" name="%s" value="%s" style="width: 100%%" autocomplete="off" placeholder="%s" />',
+			esc_attr( $type ),
+			esc_attr( $option_name ),
+			esc_attr( $option_name ),
+			esc_attr( $option_value ),
+			esc_attr( $placeholder )
+		);
+		if ( false !== $description ) {
+			echo '<p class="description">' . esc_html( $description ) . '</p>';
+		}
+	}
+
+	/**
+	 * Displays a select/drop-down input field on the options page
+	 *
+	 * @param string $option_name the name of the plugin option.
+	 * @param string $description description to display under option.
+	 * @return void
+	 */
+	public function options_page_select_input_action( $option_name, $description = false ) {
+		$option_value = get_option( $option_name, '' );
+		printf( '<select id="%s" name="%s">', esc_attr( $option_name ), esc_attr( $option_name ) );
+		wp_dropdown_roles( $option_value );
+		printf( '</select>' );
+		if ( false !== $description ) {
+			echo '<p class="description">' . esc_html( $description ) . '</p>';
+		}
+	}
+
+	/**
+	 * Sets up actions for plugin settings
+	 *
+	 * @return void
+	 */
+	public function register_settings_action() {
+		add_settings_section(
+			'ahjwtauth-sign-in-widget-options-section',
+			'',
+			null,
+			'ahjwtauth-sign-in-widget',
+		);
+
+		register_setting(
+			'ahjwtauth-sign-in-widget',
+			'ahjwtauth-private-secret',
+			array(
+				'type' => 'string',
+				'show_in_rest' => false,
+			),
+		);
+
+		register_setting(
+			'ahjwtauth-sign-in-widget',
+			'ahjwtauth-jwks-url',
+			array(
+				'type' => 'string',
+				'show_in_rest' => true,
+			),
+		);
+
+		register_setting(
+			'ahjwtauth-sign-in-widget',
+			'ahjwtauth-jwt-header',
+			array(
+				'type' => 'string',
+				'show_in_rest' => true,
+				'default' => 'Authorization',
+			),
+		);
+
+		add_settings_field(
+			'ahjwtauth-private-secret',
+			'JWT Private Secret',
+			function() {
+				$this->options_page_text_input_action(
+					'ahjwtauth-private-secret',
+					'text',
+					__( 'Paste your JWT private secret here.', 'ah-jwt-auth' ),
+					__( 'This secret is used for verifying the token (use this field or the "JWKS URL", not both).', 'ah-jwt-auth' ),
+				);
+			},
+			'ahjwtauth-sign-in-widget',
+			'ahjwtauth-sign-in-widget-options-section'
+		);
+
+		add_settings_field(
+			'ahjwtauth-jwks-url',
+			'JWKS URL',
+			function() {
+				$this->options_page_text_input_action(
+					'ahjwtauth-jwks-url',
+					'text',
+					__( 'Enter the JWKS URL to validate the JWT.', 'ah-jwt-auth' ),
+					__( 'The retreived JWKS is used for verifying the token (use this field or the "JWT Private Secret", not both)', 'ah-jwt-auth' ),
+				);
+			},
+			'ahjwtauth-sign-in-widget',
+			'ahjwtauth-sign-in-widget-options-section',
+		);
+
+		add_settings_field(
+			'ahjwtauth-jwt-header',
+			'JWT Header',
+			function() {
+				$this->options_page_text_input_action(
+					'ahjwtauth-jwt-header',
+					'text',
+					__( 'Enter the HTTP header that contains the JWT.', 'ah-jwt-auth' ),
+					__( 'The JWT will be retrieved from the specified HTTP header. This defaults to the "Authorization" header.', 'ah-jwt-auth' ),
+				);
+			},
+			'ahjwtauth-sign-in-widget',
+			'ahjwtauth-sign-in-widget-options-section',
+		);
+
+		add_settings_field(
+			'ahjwtauth-user-role',
+			'Default User Role',
+			function() {
+				$this->options_page_select_input_action(
+					'ahjwtauth-user-role',
+					__( 'Select the role for and auto-created user if a role claim is not found in the JWT.', 'ah-jwt-auth' ),
+				);
+			},
+			'ahjwtauth-sign-in-widget',
+			'ahjwtauth-sign-in-widget-options-section',
+		);
+	}
+}
---
+++ includes/class-ahjwtauthsignin.php
@@ -0,0 +1,243 @@
+<?php
+/**
+ *
+ * AhJwtAuthSignIn is the main class that handles sign-in.
+ *
+ * @file
+ * @category AhJwtAuthSignIn
+ * @package  AhJwtAuth
+ */
+
+namespace AhJwtAuth;
+
+use Exception;
+use Firebase\JWT\JWT;
+use Firebase\JWT\SignatureInvalidException;
+use Firebase\JWT\JWK;
+
+/**
+ *
+ * This class handles all the sign-in logic that extracts and verifies the JWT
+ * in the request and subsequently signs the user into WordPress.
+ *
+ * @category Class
+ * @package  AhJwtAuth
+ */
+class AhJwtAuthSignIn {
+	/**
+	 * Sets up the class ready for use
+	 *
+	 * @return void
+	 */
+	public function __construct() {
+		// Set up admin class.
+		$this->ah_jwt_auth_admin = new AhJwtAuthAdmin();
+
+		add_action( 'admin_notices', array( $this, 'ahjwtauth_admin_notice' ) );
+		add_action( 'login_head', array( $this, 'ahjwtauth_log_user_in' ) );
+	}
+
+	/**
+	 * Logs user in based on JWT
+	 *
+	 * If a valid JWT is present in the HTTP request it is verified and parsed so
+	 * the user contained in the token can be signed in
+	 *
+	 * @return void
+	 */
+	public function ahjwtauth_log_user_in() {
+		// if user is already logged in just return immediately.
+		if ( is_user_logged_in() ) {
+			return;
+		}
+
+		// get jwt.
+		$jwt = $this->get_token();
+		if ( false === $jwt ) {
+			return;
+		}
+
+		// verify JWT and grab payload.
+		$payload = $this->verify_token( $jwt );
+		if ( false === $payload ) {
+			return;
+		}
+
+		// If we cannot extract the user's email from header this is an error.
+		if ( ! isset( $payload->email ) ) {
+			$this->error = __( 'AH JWT Auth expects email attribute to identify user, but it does not exist in the JWT. Please check your reverse proxy configuration', 'ah-jwt-auth' );
+			return;
+		}
+		$email = $payload->email;
+
+		$user = get_user_by( 'email', $email );
+
+		if ( ! $user ) {
+			$random_password = wp_generate_password( 64, false );
+			$user_id = wp_create_user( $email, $random_password, $email );
+			$user = get_user_by( 'id', $user_id );
+
+			// set role on creation to configured default if not included in jwt.
+			if ( ! isset( $payload->role ) ) {
+				$user->set_role( get_option( 'ahjwtauth-user-role', 'subscriber' ) );
+			}
+		}
+
+		// If we can extract the user's role from the JWT, then set the role, otherwise leave as-is.
+		if ( isset( $payload->role ) ) {
+			$user->set_role( strtolower( $payload->role ) );
+		}
+
+		wp_clear_auth_cookie();
+		wp_set_current_user( $user->ID );
+		wp_set_auth_cookie( $user->ID );
+		do_action( 'wp_login', $user->login, $user );
+
+		// redirect after login.
+		$redirect_url = home_url();
+		if ( current_user_can( 'manage_options' ) ) {
+			$redirect_url = admin_url();
+		}
+		wp_safe_redirect( isset( $_GET['redirect_to'] ) ? wp_unslash( $_GET['redirect_to'] ) : $redirect_url );
+		exit;
+	}
+
+	/**
+	 * Prints any admin notices
+	 *
+	 * If any warning or error values have been set, they are printed using this function
+	 *
+	 * @return void
+	 */
+	public function ahjwtauth_admin_notice() {
+		if ( isset( $this->error ) ) {
+			$class = 'notice notice-error';
+			$message = $this->error;
+			printf( '<div class="%1$s"><p>%2$s</p></div>', esc_attr( $class ), esc_html( $message ) );
+		}
+
+		if ( isset( $this->warning ) ) {
+			$class = 'notice notice-warning is-dismissible';
+			$message = $this->warning;
+			printf( '<div class="%1$s"><p>%2$s</p></div>', esc_attr( $class ), esc_html( $message ) );
+		}
+	}
+
+	/**
+	 * Retrieves the JWT
+	 *
+	 * The JWT is retrieved from the configured HTTP request header
+	 *
+	 * A value of false is returned on error
+	 *
+	 * @return string the payload from the JWT
+	 */
+	private function get_token() {
+		$jwt_header = $this->get_header();
+		if ( ! isset( $_SERVER[ $jwt_header ] ) ) {
+			$this->warning = __( 'AH JWT Auth is enabled, but the expected JWT was not found. Please double check your reverse proxy configuration', 'ah-jwt-auth' );
+			return false;
+		}
+
+		// Handle "Header: Bearer <JWT>" form by stipping the "Bearer " prefix.
+		$array = explode( ' ', sanitize_text_field( wp_unslash( $_SERVER[ $jwt_header ] ) ) );
+		if ( 'Bearer' == $array[0] ) {
+			array_shift( $array );
+		}
+
+		return implode( ' ', $array );
+	}
+
+	/**
+	 * Decodes the JWT
+	 *
+	 * The provided JWT is verified using the configured key and decoded
+	 *
+	 * A value of false is returned on error
+	 *
+	 * @param string $jwt the JWT to decode.
+	 * @return object the payload from the JWT
+	 */
+	private function verify_token( $jwt ) {
+		$key = $this->get_key();
+		if ( false === $key ) {
+			return false;
+		}
+		try {
+			$payload = JWT::decode( $jwt, $key, array( 'RS256', 'HS256' ) );
+		} catch ( SignatureInvalidException $e ) {
+			$this->error = __( 'AH JWT Auth cannot verify the JWT. Please double check that your private secret or JWKS URL is configured correctly', 'ah-jwt-auth' );
+			return false;
+		} catch ( Exception $e ) {
+			return false;
+		}
+		return $payload;
+	}
+
+	/**
+	 * Returns the key to verify the JWT
+	 *
+	 * Depending on the configuration of the plugin, this function will return
+	 * the static key used to verify the JWT or will retrieve a JSON Web Key Set (JWKS)
+	 * from the configured URL
+	 *
+	 * A value of false is returned on error
+	 *
+	 * @return string the key used for verifying the signature of the JWT
+	 */
+	private function get_key() {
+		$jwks_url = get_option( 'ahjwtauth-jwks-url' );
+		if ( '' !== $jwks_url ) {
+			// retrieve json from JWKS URL with caching.
+			$json = get_transient( 'ahjwtauth_jwks_json' );
+
+			// if transient did not exist, attempt to get url.
+			if ( false === $json ) {
+				$response = wp_remote_get( $jwks_url );
+				if ( is_wp_error( $response ) ) {
+					$this->error = __( 'AH JWT Auth: error retrieving the JWKS URL', 'ah-jwt-auth' );
+					return false;
+				}
+
+				$json = wp_remote_retrieve_body( $response );
+			}
+			if ( '' == $json ) {
+				$this->error = __( 'AH JWT Auth could not retrieve the specified JWKS URL', 'ah-jwt-auth' );
+				return false;
+			}
+			// try to decode json.
+			$jwks = @json_decode( $json, true );
+			if ( null === $jwks ) {
+				$this->error = __( 'AH JWT Auth cannot decode the JSON retrieved from the JWKS URL', 'ah-jwt-auth' );
+				return false;
+			}
+			// cache json for future.
+			set_transient( 'ahjwtauth_jwks_json', $json, 60 * 240 );
+
+			// parse the JWKS response.
+			try {
+				$key = JWK::parseKeySet( $jwks );
+			} catch ( Exception $e ) {
+				$this->error = $e->getMessage();
+				return false;
+			}
+		} else {
+			// otherwise use shared secret.
+			$key = get_option( 'ahjwtauth-private-secret' );
+		}
+
+		return $key;
+	}
+
+	/**
+	 * Returns a header in "HTTP" form into a form usable with $_SERVER['HEADER']
+	 *
+	 * The returned string is done by converting the configured settingto uppercase,
+	 * replacing "-" with "_" and adding the prefix of "HTTP_".
+	 *
+	 * @return string the header name usable with _$SERVER
+	 */
+	private function get_header() {
+		return 'HTTP_' . str_replace( '-', '_', strtoupper( get_option( 'ahjwtauth-jwt-header' ) ) );
+	}
+}
